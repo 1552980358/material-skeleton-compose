0d1822396e8706c6fc6b0eabcc6287f858e56c1e
1552980358 <1552980358@qq.com>
2024-09-21 20:01:39 +0800

    [example] Add plugin & library dependency `kotlinx-serialization`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.compose.compiler) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
 }
 
 allprojects {
